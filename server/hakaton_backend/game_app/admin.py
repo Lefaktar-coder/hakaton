fb11c537d3a53285e21f0f88e94bb3f4b445e6bf
lefaktar <lefaktar@yandex.ru>
2023-11-12 09:29:27 +0300

    create-login-auth-v2

--- server/hakaton_backend/game_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Fears
-
-
-class FearsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'src', 'descr', 'title')
-
-
-admin.site.register(Fears, FearsAdmin)
--- server/hakaton_backend/game_app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Fears(models.Model):
-    title = models.CharField(max_length=200)
-    descr = models.TextField()
-    src = models.ImageField('Картинка',
-                            upload_to='fears/',)
