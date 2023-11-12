050aab89c07cbfdce137a780eb2d8e5f3117e729
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2023-11-12 11:59:17 +0400

    fix styles in gamezone

--- client/src/components/fear/Fear.jsx
+++ client/src/components/fear/Fear.jsx
@@ -1,81 +1,84 @@
 /* eslint-disable react/prop-types */
-import gsap from 'gsap'
-import { useEffect, useRef, useState } from 'react'
-import { EMOJIS } from '../../utils/constants'
-import './Fear.css'
+import gsap from "gsap";
+import { useEffect, useRef, useState } from "react";
+import { EMOJIS } from "../../utils/constants";
+import "./Fear.css";
 
 const Fear = ({ points, delay, speed, onWhack, pointsMin = 10 }) => {
-	const buttonRef = useRef(null)
-	const bobRef = useRef(null)
-	const pointsRef = useRef(points)
-	const [whacked, setWhacked] = useState(false)
-	const [emoji, setEmoji] = useState(getRandomEmoji())
+  const buttonRef = useRef(null);
+  const bobRef = useRef(null);
+  const pointsRef = useRef(points);
+  const [whacked, setWhacked] = useState(false);
+  const [emoji, setEmoji] = useState(getRandomEmoji());
 
-	function getRandomEmoji() {
-		const randomIndex = Math.floor(Math.random() * EMOJIS.length)
-		return EMOJIS[randomIndex]
-	}
+  function getRandomEmoji() {
+    const randomIndex = Math.floor(Math.random() * EMOJIS.length);
+    return EMOJIS[randomIndex];
+  }
 
-	const whack = () => {
-		setWhacked(true)
-		onWhack(pointsRef.current)
-	}
+  const whack = () => {
+    setWhacked(true);
+    onWhack(pointsRef.current);
+  };
 
-	const initializeAnimation = () => {
-		gsap.set(buttonRef.current, {
-			yPercent: 100,
-			display: 'block',
-		})
+  const initializeAnimation = () => {
+    gsap.set(buttonRef.current, {
+      yPercent: 50,
+      display: "block",
+    });
 
-		bobRef.current = gsap.to(buttonRef.current, {
-			yPercent: 0,
-			duration: speed,
-			yoyo: true,
-			repeat: -3,
-			delay,
-			repeatDelay: delay,
-			onRepeat: () => {
-				pointsRef.current = Math.floor(Math.max(pointsRef.current * 0.9, pointsMin))
-			},
-		})
-	}
+    bobRef.current = gsap.to(buttonRef.current, {
+      yPercent: 300,
+      duration: speed,
+      yoyo: true,
+      repeat: -3,
+      delay,
+      repeatDelay: delay,
+      onRepeat: () => {
+        pointsRef.current = Math.floor(
+          Math.max(pointsRef.current * 0.9, pointsMin)
+        );
+      },
+    });
+  };
 
-	useEffect(() => {
-		initializeAnimation()
+  useEffect(() => {
+    initializeAnimation();
 
-		return () => {
-			bobRef.current?.kill()
-		}
-	}, [delay, speed, pointsMin])
+    return () => {
+      bobRef.current?.kill();
+    };
+  }, [delay, speed, pointsMin]);
 
-	useEffect(() => {
-		if (whacked) {
-			pointsRef.current = points
-			bobRef.current.pause()
-			gsap.to(buttonRef.current, {
-				yPercent: 100,
-				duration: 0.1,
-				onComplete: () => {
-					gsap.delayedCall(gsap.utils.random(1, 3), () => {
-						setWhacked(false)
-						setEmoji(getRandomEmoji())
-						bobRef.current?.restart()
-					})
-				},
-			})
-		}
-	}, [whacked])
+  useEffect(() => {
+    if (whacked) {
+      pointsRef.current = points;
+      bobRef.current.pause();
+      gsap.to(buttonRef.current, {
+        yPercent: 300,
+        duration: 0.1,
+        onComplete: () => {
+          gsap.delayedCall(gsap.utils.random(1, 3), () => {
+            setWhacked(false);
+            setEmoji(getRandomEmoji());
+            bobRef.current?.restart();
+          });
+        },
+      });
+    }
+  }, [whacked]);
 
-	return (
-		<div className='fear-hole'>
-			<button
-				className={`fear ${whacked ? 'whacked' : ''}`}
-				ref={buttonRef}
-				onClick={whack}>
-				{emoji}
-			</button>
-		</div>
-	)
-}
+  return (
+    <div className="fear-hole">
+      <button
+        className={`fear ${whacked ? "whacked" : ""}`}
+        ref={buttonRef}
+        onClick={whack}
+      >
+        {emoji}
+      </button>
+    </div>
+  );
+};
 
-export default Fear
+export default Fear;
