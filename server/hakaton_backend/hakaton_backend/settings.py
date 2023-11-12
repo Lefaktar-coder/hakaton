b2316029c5ca86362d3ed0535b27169e6b3d2f8d
Marat Agliullin <marat.agl@gmail.com>
2023-11-12 12:44:55 +0300

    set life time JWT token 30 day

--- server/hakaton_backend/hakaton_backend/settings.py
+++ server/hakaton_backend/hakaton_backend/settings.py
@@ -150,7 +150,7 @@ CORS_ALLOWED_ORIGINS = [
 
 SIMPLE_JWT = {
     # Устанавливаем срок жизни токена
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
